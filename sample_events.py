d718b6381ff48e63369349620e7384ed5ef35185
Jukka Huhtamäki <jukka.huhtamaki@tut.fi>
2019-01-31 15:36:54 +0200

    Reading a set of sample events

--- sample_events.py
+++ sample_events.py
@@ -1,1 +1,13 @@
 import requests
+import json
+
+req = requests.get('https://visittampere.fi:443/api/v1/event?limit=10')
+
+print(dir(req))
+
+# we then print out the http status_code that was returned on making this request
+# you should see a successfull '200' code being returned.
+print(req.status_code)
+
+with open('data.json', 'w') as f:
+  json.dump(req.json(), f)
